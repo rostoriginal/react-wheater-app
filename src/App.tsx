ef43d22e42696189afdb0cddaadc74c8946c5085
rostyslav.butsyk <rostyslav.butsyk@gmail.com>
2023-06-20 14:40:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-white w-[410px] min-h-[680px] mx-auto mt-20 rounded-3xl px-3 py-3">
+       
+      </div>
+    </>
+  );
+}
+
+export default App;
